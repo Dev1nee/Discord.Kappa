bcca309beebdac22cc785d87610056c38cd450ce
Dom <32180099+Dev1nee@users.noreply.github.com>
2017-11-05 13:09:00 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const discordKappa = require('./src/index.js');
 console.log('Starting tests');
 console.log('First test - kappa function')
 console.log(discordKappa.kappa());
-console.log('help func')
+console.log('Second test - help func')
 console.log(discordKappa.help());
