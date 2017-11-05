2f67c7c4ca175a0eae6b856591c2fd69d21df294
moosecoop <cooper.makhijani@icloud.com>
2017-11-04 21:24:36 -0700

    created test

---
+++ test.js
@@ -0,0 +1,7 @@
+const discordKappa = require('./src/index.js');
+
+console.log('Starting tests');
+console.log('First test - kappa function')
+console.log(discordKappa.kappa());
+console.log('help func')
+console.log(discordKappa.help());
